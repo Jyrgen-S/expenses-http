309cad0026dcd99964917e47e6f7c2e1f80c0dfe
jurgen <revisto.rourke@gmail.com>
2024-01-26 15:29:45 +0200

    tooleht 2

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,15 @@ import './ExpenseItem.css'
 
 function ExpenseItem() {
     return (
-    <div>
+        <div className="expense-Item">
         <div>Date</div>
+        <div className="expense-item__description">
+        </div>
         <h2>Title</h2>
+        <div className="expense-item__price">
         <div>Amount</div>
     </div>
-
+</div>
 )
 }
 
