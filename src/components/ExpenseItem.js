514d739e8f9056397da00972c4950fb22a7ee697
jurgen <revisto.rourke@gmail.com>
2024-01-28 17:38:43 +0200

    Uued muutujad

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,19 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
 function ExpenseItem() {
-    return (
-        <div className="expense-Item">
-        <div>Date</div>
-        <div className="expense-item__description">
-        </div>
-        <h2>Title</h2>
-        <div className="expense-item__price">
+  const date = new date(2023, 9, 6);
+  const title = "New book";
+  const price = 30.99;
+  return (
+    <div className="expense-Item">
+      <div>Date</div>
+      <div className="expense-item__description"></div>
+      <h2>Title</h2>
+      <div className="expense-item__price">
         <div>Amount</div>
+      </div>
     </div>
-</div>
-)
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
