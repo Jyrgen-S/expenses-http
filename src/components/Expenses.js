b8381036371f48a9c2beb4edbc0d3b0d051f1fb7
jurgen <revisto.rourke@gmail.com>
2024-01-30 14:50:45 +0200

    Komponentide kompositsioon ja info edastamine

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,13 @@
 import "./Expenses.css";
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem expenseData={props.expenses[0]} />
       <ExpenseItem expenseData={props.expenses[1]} />
-    </div>
+    </Card>
   );
 }
 
