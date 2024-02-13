7ae28f6c0b1451566d7656114a63697fc3085f8c
jurgen <revisto.rourke@gmail.com>
2024-02-13 15:22:21 +0200

    Ylesanne 4 - Andmete suunamine (bottom-up) ja vaartuse hetkeseisu lugemine ning maaramine

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,33 @@
+import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2023");
+
+  const handleYearChange = (year) => {
+    setSelectedYear(year);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    <ExpensesFilter
+      selectedYear={selectedYear}
+      onYearChange={handleYearChange}
+    />;
+
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
   return (
     <Card className="expenses">
+      <ExpensesFilter onYearChange={handleYearChange} />
       <ExpenseItem expenseData={props.expenses[0]} />
       <ExpenseItem expenseData={props.expenses[1]} />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem key={expense.id} expenseData={expense} />
+      ))}
     </Card>
   );
 };
