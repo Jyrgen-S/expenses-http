c638210ac1fe65b967fa2ed68b7aa38dc2ef45d1
jurgen <revisto.rourke@gmail.com>
2024-02-19 14:03:27 +0200

    ylesanne 5 - aasta filtreerimine ja filtreeritud andmete kuvamine

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,44 +5,30 @@ import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 
 const Expenses = (props) => {
-  const filterChangeHandler = (filteredYear) => {
-    console.log("Year data in Expenses.js " + filteredYear);
-  };
-  /*
-  props.expenses.map((expense) => {
-    console.log(expense);
-  });
-  /* const [selectedYear, setSelectedYear] = useState("2023");
+  const [selectedYear, setSelectedYear] = useState("2023");
 
-  const handleYearChange = (year) => {
-    setSelectedYear(year);
+  const filterChangeHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
   };
 
   const filteredExpenses = props.expenses.filter((expense) => {
-    <ExpensesFilter
-      selectedYear={selectedYear}
-      onYearChange={handleYearChange}
-    />;
-
     return expense.date.getFullYear().toString() === selectedYear;
-  });*/
+  });
 
   return (
     <Card className="expenses">
-      <ExpensesFilter onChangeFilter={filterChangeHandler} />
-      {props.expenses.map((expense) => {
-        return <ExpenseItem expenseData={expense} key={expense.id} />;
-      })}
+      <ExpensesFilter
+        selectedYear={selectedYear}
+        onYearChange={filterChangeHandler}
+      />
+      {filteredExpenses.length > 0 ? (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem key={expense.id} expenseData={expense} />
+        ))
+      ) : (
+        <p>No expenses found for the selected year.</p>
+      )}
     </Card>
-    /*
-    <Card className="expenses">
-      <ExpensesFilter onYearChange={handleYearChange} />
-      <ExpenseItem expenseData={props.expenses[0]} />
-      <ExpenseItem expenseData={props.expenses[1]} />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem key={expense.id} expenseData={expense} />
-      ))} 
-    </Card> */
   );
 };
 
